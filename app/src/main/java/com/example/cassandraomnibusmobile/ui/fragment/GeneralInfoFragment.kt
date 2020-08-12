e2a4ebdd1d6b684f3c961437d395e2bdac935931
Brian Batey <batey.18@osu.edu>
2020-08-11 23:31:21 -0400

    GenInfo Page (Version 1) Complete Patch Notes need to be linked

--- app/src/main/java/com/example/cassandraomnibusmobile/ui/fragment/GeneralInfoFragment.kt
+++ app/src/main/java/com/example/cassandraomnibusmobile/ui/fragment/GeneralInfoFragment.kt
@@ -1,13 +1,10 @@
 package com.example.cassandraomnibusmobile.ui.fragment
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.cassandraomnibusmobile.R
 
@@ -25,7 +22,7 @@ class GeneralInfoFragment : Fragment(), View.OnClickListener {
 
         val v = inflater.inflate(R.layout.gen_info_layout, container, false)
 
-        tView = v.findViewById(R.id.patch_notes_tview)
+        tView = v.findViewById(R.id.patch_notes_t_view)
 
         tView.setOnClickListener(this)
 
@@ -34,7 +31,7 @@ class GeneralInfoFragment : Fragment(), View.OnClickListener {
 
     override fun onClick(v: View) {
         when(v.id){
-            R.id.patch_notes_tview ->{
+            R.id.patch_notes_t_view ->{
                 //Display patch notes fragment
                 //Grey out the color of the text a bit maybe to represent click
             }
